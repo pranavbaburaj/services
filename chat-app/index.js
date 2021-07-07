0b193758c7c5c249a13b762123234bba6a7e128b
Pranav Baburaj <diamonddevstuff@gmail.com>
2021-07-07 18:39:34 +0000

    fmt

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,33 +1,32 @@
 const express = require("express");
 const http = require("http");
-const socket = require('socket.io');
-const os = require('os');
-
+const socket = require("socket.io");
+const os = require("os");
+const { DefaultDeserializer } = require("v8");
 
 const app = express();
 
-app.use(express.static("public"))
-
+app.use(express.static("public"));
 
 var server = app.listen(process.env.PORT || 5000, () => {
-    console.log(`server started on port ${process.env.PORT || 5000}`)
-})
+  console.log(`server started on port ${process.env.PORT || 5000}`);
+});
 
-var io = socket(server) 
-console.log("hello")
-var io = socket(server)
+var io = socket(server);
+console.log("hello");
+var io = socket(server);
 
 io.on("connection", (socket) => {
-  console.log(`connected to ${socket.id}`)
+  console.log(`connected to ${socket.id}`);
 
   socket.on("chat", (data) => {
-    console.log(data)
+    console.log(data);
     new_data = {
-        message : data.message,
-        handle : data.handle,
-        person : "anonymous"
-    }
+      message: data.message,
+      handle: data.handle,
+      person: "anonymous",
+    };
 
-    io.sockets.emit('chat', new_data)
-  })
-})
+    io.sockets.emit("chat", new_data);
+  });
+});
--- chat-app/public/static/js/main.js
+++ chat-app/public/static/js/main.js
@@ -1,35 +1,38 @@
-var socket = io.connect(window.location.host)
+var socket = io.connect(window.location.host);
 
 var output = document.querySelector(".message-space");
 var send = document.querySelector(".send");
 var hello = document.querySelector(".message");
 
-
-
-
-send.addEventListener('click', () => {
-  if(hello.value.length.trim() == 0){return null}
+send.addEventListener("click", () => {
+  if (hello.value.length.trim() == 0) {
+    return null;
+  }
   socket.emit("chat", {
-    message : hello.value,
-    handle : "daat",
-  })
-  hello.value = ""
-  console.log(window.location.host)
-})
+    message: hello.value,
+    handle: "daat",
+  });
+  hello.value = "";
+  console.log(window.location.host);
+});
 
 socket.on("chat", (data) => {
-  console.log(data)
-  output.innerHTML += '<div class="hello"><p>' + data.message +  '</p><span class="time-right">' + data.person + '</span></div><br>'
-})
+  console.log(data);
+  output.innerHTML +=
+    '<div class="hello"><p>' +
+    data.message +
+    '</p><span class="time-right">' +
+    data.person +
+    "</span></div><br>";
+});
 
-
-hello.addEventListener('keydown', function(e) {
+hello.addEventListener("keydown", function (e) {
   if (e.keyCode == 13) {
-      socket.emit("chat", {
-        message : hello.value,
-        handle : "daat",
-      })
-      hello.value = ""
-      console.log(window.location.host)
+    socket.emit("chat", {
+      message: hello.value,
+      handle: "daat",
+    });
+    hello.value = "";
+    console.log(window.location.host);
   }
-})
+});
