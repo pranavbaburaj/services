5a3882cbbab090dce8136c212222a93059b0a021
Pranav Baburaj <diamonddevstuff@gmail.com>
2021-07-17 07:48:59 +0000

    fmt

--- index.ts
+++ index.ts
@@ -1,24 +1,24 @@
-import cookieParser from 'cookie-parser'
-import express, { Response, Request } from 'express'
-import { join } from 'path'
+import cookieParser from "cookie-parser";
+import express, { Response, Request } from "express";
+import { join } from "path";
 
-const application = express()
-const PORT = process.env.PORT || 3000
+const application = express();
+const PORT = process.env.PORT || 3000;
 
-const TEMPLATE_PATH = join(__dirname, "public")
-application.use(express.static(TEMPLATE_PATH))
-application.use(cookieParser())
+const TEMPLATE_PATH = join(__dirname, "public");
+application.use(express.static(TEMPLATE_PATH));
+application.use(cookieParser());
 
-application.get("/", (request:Request, response:Response) => {
-    response.sendFile(join(TEMPLATE_PATH, "index.html"))
-})
+application.get("/", (request: Request, response: Response) => {
+  response.sendFile(join(TEMPLATE_PATH, "index.html"));
+});
 
-application.get("/join/:room", (request: Request, response:Response):void => {
-    const roomId = request.params.room
-    response.cookie('roomId', roomId)
-    response.send(roomId)
-})
+application.get("/join/:room", (request: Request, response: Response): void => {
+  const roomId = request.params.room;
+  response.cookie("roomId", roomId);
+  response.send(roomId);
+});
 
 application.listen(PORT, () => {
-    console.log("Check out port " + PORT)
-})
+  console.log("Check out port " + PORT);
+});
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,21 +1,25 @@
-import EventListeners from "./listeners.js"
+import EventListeners from "./listeners.js";
 
 const createUUID = () => {
-    let date = new Date().getTime()
-    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        const r = (date + Math.random()*16)%16 | 0
-        date = Math.floor(date/16)
-        return (c=='x' ? r :(r&0x3|0x8)).toString(16)
-    })
-    return uuid
-}
+  let date = new Date().getTime();
+  const uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+    /[xy]/g,
+    function (c) {
+      const r = (date + Math.random() * 16) % 16 | 0;
+      date = Math.floor(date / 16);
+      return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+    }
+  );
+  return uuid;
+};
 
 const createRoom = () => {
-    const roomUUID = createUUID()
-    window.location.href = "/join/" + roomUUID
-}
+  const roomUUID = createUUID();
+  window.location.href = "/join/" + roomUUID;
+};
 
-const createRoomButton = document.querySelector('.create-new-room')
-const listeners = new EventListeners(createRoomButton, new Map([
-    ['click', createRoom]
-]))
+const createRoomButton = document.querySelector(".create-new-room");
+const listeners = new EventListeners(
+  createRoomButton,
+  new Map([["click", createRoom]])
+);
